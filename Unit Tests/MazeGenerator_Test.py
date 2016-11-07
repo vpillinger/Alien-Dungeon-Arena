029337eade1fe111eefe8191d26d743fce29e4c7
vincent pill <vincent pill>
2016-11-07 18:23:17 -0500

    Started unit tests for maze generator, added tests for size and dimensions, and started tests for graph completeness.

--- Unit Tests/MazeGenerator_Test.py
+++ Unit Tests/MazeGenerator_Test.py
@@ -1,10 +1,43 @@
-import unittest
+import unittest, MazeGenerator, numpy as np, queue
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
+    # Size & Length Tests
+    def test_size_0(self):
+        """Test whether generate_maze returns an empty numpy array on size 0"""
+        maze = MazeGenerator.generate_maze(0)
+        self.assertEqual(0, maze.size)
+        self.assertEqual((0,), maze.shape)
+
+    def test_size(self):
+        """Test whether generate_maze returns a numpy array of the proper size
+        and dimensions for several inputs"""
+        lengths = [1,5,10]
+        for i in lengths:
+            maze = MazeGenerator.generate_maze(i)
+            self.assertEqual(i*i*4, maze.size)
+            self.assertEqual((i,i,4), maze.shape)
+
+    # Graph completeness tests
+    def test_completeness(self):
+        """Use Breadth-First-Search to test that maze graph is complete (all nodes connected)
+        The number of visited nodes should be equal to the maze's size"""
+        lengths = [1,5,10,20,50]
+        for length in lengths:
+            maze = MazeGenerator.generate_maze(length)
+            q = queue.Queue(maze.size)
+            # BFS
+            visited = set()
+            q.put((0,0))
+            while not q.empty():
+                # pop to visited
+                visited.add(q._get())
+                # get neighbors
+                print ('get neighbors')
+                # add neighbors to queue
+            # not implemented yet
+            #self.assertEqual(maze.size, len(visited))
 
 if __name__ == '__main__':
     unittest.main()
