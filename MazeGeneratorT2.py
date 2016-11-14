0d60ac2dbea08fba40a986f3e16ada600ce800e7
vincent pill <vincent pill>
2016-11-14 08:08:44 -0500

    Added a new version of maze generator which is about 2x faster than numpy.

---
+++ MazeGeneratorT2.py
@@ -0,0 +1,91 @@
+import random
+
+def generate_maze(length):
+
+    def walk(maze, cur):
+        """Randomly connect this node to an unvisited neighbor.
+        """
+
+        random.shuffle(directions)
+        for dir in directions:
+            n = (cur[0] + dir[0], cur[1] + dir[1])
+            if (n[0] >= 0 and n[0] < len(maze)) and (n[1] >= 0 and n[1] < len(maze)) \
+                    and (n not in visited):
+                maze[cur[0]][cur[1]].append(n)
+                maze[n[0]][n[1]].append(cur)
+                visited.add(n)
+                visited.add(cur)
+                return n
+        return None
+
+    def hunt_walk(maze, cur):
+        """Randomly connect this node to an visited neighbor.
+        """
+
+        random.shuffle(directions)
+        for dir in directions:
+            n = (cur[0] + dir[0], cur[1] + dir[1])
+            if (n[0] >= 0 and n[0] < len(maze)) and (n[1] >= 0 and n[1] < len(maze)) \
+                    and (n in visited):
+                maze[cur[0]][cur[1]].append(n)
+                maze[n[0]][n[1]].append(cur)
+                visited.add(cur)
+                return cur
+        return None
+
+    def hunt(maze, skip_rows = 0):
+        """Top->bottom left->right search for an unvisited node adjacent to a visited node
+        and connect them
+        """
+
+        cur = None
+        for i in range(skip_rows, len(maze)): # enumerate with slice doesn't work here?
+            row_count = 0
+            for j, cell in enumerate(maze[i]):
+                if len(cell) == 0:
+                    cur = hunt_walk(maze, (i, j))
+                else:
+                    row_count += 1
+                if cur is not None:
+                    return cur, skip_rows
+            if(row_count == len(maze)): # if every node was visited already
+                skip_rows += 1
+        return None, skip_rows
+
+    #---- Method Start ----
+
+    visited = set()
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    if(length == 0):
+        return [[[]]]
+    maze = [[[] for j in range(length)] for i in range(length)]
+    cur, done_rows = (random.randrange(length), random.randrange(length)), 0
+    while 1:
+        cur = walk(maze, cur)
+        if cur is None:
+            cur, done_rows = hunt(maze, done_rows)
+            if cur is None:
+                break
+    return maze
+
+def print_maze(maze):
+
+    line = ""
+    for row in maze:  # top of maze
+        line += " _"
+    print(line)
+
+    # We only need to do one side of each connection: so start in top-left look east&south.
+    for i, row in enumerate(maze):
+        line = "|" # left of maze
+        for j, cell in enumerate(row):
+            if (i+1, j) in cell:
+                line += " "
+            else:
+                line += "_"
+            if (i, j+1) in cell:
+                line += " "
+            else:
+                line += "|"
+        print(line)
---
+++ Unit Tests/MazeGeneratorT2_Test.py
@@ -0,0 +1,51 @@
+import unittest, MazeGeneratorT2
+from collections import deque
+
+
+class MyTestCase(unittest.TestCase):
+
+    # Size & Length Tests
+    def test_size_0(self):
+        """Test whether generate_maze returns an empty numpy array on size 0"""
+
+        maze = MazeGeneratorT2.generate_maze(0)
+        self.assertEqual(1, len(maze))
+        self.assertEqual(1, len(maze[0]))
+        self.assertEqual(0, len(maze[0][0]))
+
+    def test_size(self):
+        """Test whether generate_maze returns a numpy array of the proper size
+        and dimensions for several inputs"""
+
+        lengths = [1,5,10]
+        for i in lengths:
+            maze = MazeGeneratorT2.generate_maze(i)
+            self.assertEqual(i, len(maze))
+            self.assertEqual((i), len(maze[0]))
+
+    # Graph completeness tests
+    def test_completeness(self):
+        """Use Breadth-First-Search to test that maze graph is complete (all nodes connected)
+        The number of visited nodes should be equal to the maze's size"""
+
+        lengths = [1,5,20,50,100,1000]
+        directions = (-1, 0), (0, 1),(1, 0), (0, -1)
+
+        for length in lengths:
+            maze = MazeGeneratorT2.generate_maze(length)
+            q = deque()
+            visited = set()
+            q.append((0,0))
+            while q:
+                # pop to visited
+                current = q.popleft()
+                visited.add(current)
+                # add neighbors to queue
+                for neighbor in maze[current[0]][current[1]]:
+                    if neighbor not in visited:
+                        q.append(neighbor)
+            self.assertEqual(len(maze)**2, len(visited))
+
+# BFS
+if __name__ == '__main__':
+    unittest.main()
