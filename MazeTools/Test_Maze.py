453055828a415541b9c66c211aeb9df614f364df
vincent pill <vincent pill>
2016-11-16 19:23:47 -0500

    Created MazeTools package and added files for Maze.

--- MazeTools/Test_Maze.py
+++ MazeTools/Test_Maze.py
@@ -1,9 +1,14 @@
 import unittest
+from MazeTools import Maze as maze
 
+class MazeTest(unittest.TestCase):
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_importMaze(self):
+        self.assertEquals(False, True)
+
+    def test_generateMaze(self):
+        maze = None
+        self.assertEquals(maze is not None)
 
 
 if __name__ == '__main__':
